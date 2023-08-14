6504c2356a4b943d8d58a906f14a8ac4863c58e1
Rithik Singla <97961911+Rithik15@users.noreply.github.com>
2023-08-14 10:08:27 +0530

    Add files via upload

---
+++ GameImages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddOutGame.Shared
+{
+    public  class GameImages
+    {
+
+        public string id { get; set; }  
+        public string Image1 { get; set; }
+        public string Image2 { get; set; }  
+        public string Image3 { get; set; }  
+        public string Image4 { get; set; }  
+        public string Result { get; set; }
+
+    }
+}
